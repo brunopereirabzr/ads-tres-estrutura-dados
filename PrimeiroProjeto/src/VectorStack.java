5d49ede07393bc0197bdb79293566bf9b43af491
Bruno Pereira <brunopereira@acad.ifma.edu.br>
2024-11-29 20:42:08 -0300

    Create VectorStack.java

---
+++ PrimeiroProjeto/src/VectorStack.java
@@ -0,0 +1,36 @@
+public class VectorStack implements  IStack{
+    int capacidade;
+    String s[];
+
+    int size = 0;
+
+    public VectorStack(int capacidade){
+        this.capacidade = capacidade;
+        S = new String[capacidade];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public String top() {
+        return "";
+    }
+
+    @Override
+    public void push(String element) {
+
+    }
+
+    @Override
+    public String pop() {
+        return "";
+    }
+}
