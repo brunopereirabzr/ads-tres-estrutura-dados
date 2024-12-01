10a2687217908f042ce49575d1a96cde713df2a5
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-12-01 09:08:00 -0300

    Lista 01

---
+++ EDExercicios/src/ifma/lista01/Aluno.java
@@ -0,0 +1,13 @@
+package ifma.lista01;
+
+public class Aluno {
+    private int codigo;
+    private String matricula;
+    private String nome;
+
+    Aluno(int codigo, String matricula, String nome) {
+        this.codigo = codigo;
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+}
