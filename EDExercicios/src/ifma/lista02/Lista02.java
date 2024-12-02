028122d4886ce941349c4e3595f9d58e90a62a1f
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-12-02 03:13:08 -0300

    Lista 02

---
+++ EDExercicios/src/ifma/lista02/Lista02.java
@@ -0,0 +1,38 @@
+package ifma.lista02;
+
+public class Lista02 {
+    public static void main(String[] args) {
+        q01();
+        q02();
+    }
+
+    public static void q01(){
+        int matriz[][] = {  {1,2,3},
+                            {4,5,6},
+                            {7,8,9}};
+        for(int i=0;i< matriz.length;i++){
+            for(int j=0; j < matriz.length;j++){
+                if(i==j){
+                    matriz[i][j] = 0;
+                }
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }//fim q01
+
+    public static void q02(){
+        int matriz[][] = {  {1,2,3},
+                            {4,5,6},
+                            {7,8,9}};
+        for(int i=0;i< matriz.length;i++){
+            for(int j=0; j < matriz.length;j++){
+                if(i==j){
+                    matriz[i][j] = 0;
+                }
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }//fim q02
+}
