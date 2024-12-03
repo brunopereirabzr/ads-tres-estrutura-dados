39a9a7f4a5d511cecae177da925a528ffab36790
Bruno Pereira <brunopereirabzrdev@gmail.com>
2024-12-03 19:07:10 -0300

    Update Lista02.java

--- EDExercicios/src/ifma/lista02/Lista02.java
+++ EDExercicios/src/ifma/lista02/Lista02.java
@@ -22,9 +22,10 @@ public class Lista02 {
     }//fim q01
 
     public static void q02(){
-        int matriz[][] = {  {1,2,3},
-                            {4,5,6},
-                            {7,8,9}};
+        int matriz[][] = {  {1,2,3}, // 02
+                            {4,5,6}, // 11
+                            {7,8,9}}; //20
+        
         for(int i=0;i< matriz.length;i++){
             for(int j=0; j < matriz.length;j++){
                 if(i==j){
